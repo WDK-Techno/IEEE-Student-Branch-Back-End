6f5c64329423f380931f1403b4c009df66f039fa
Aasath <aasadh2000@gmail.com>
2024-08-05 23:28:51 +0530

    tpye errror fixed

--- src/main/java/com/IEEEUWUSB/IEEEStudentBranchBackEnd/repo/UserRoleDetailsRepo.java
+++ src/main/java/com/IEEEUWUSB/IEEEStudentBranchBackEnd/repo/UserRoleDetailsRepo.java
@@ -11,6 +11,6 @@ public interface UserRoleDetailsRepo extends JpaRepository<UserRoleDetails,Integ
     @Query("SELECT urd FROM UserRoleDetails urd WHERE urd.user = :user AND urd.isActive = :isActive AND urd.type = :type")
     Optional<UserRoleDetails> findByUserAndIsActiveAndType(User user, boolean isActive, String type);
 
-    @Query("SELECT urd FROM UserRoleDetails urd WHERE urd.user = :user AND urd.isActive = :isActive AND (urd.type = :type1 OR urd.type = :type2)")
+    @Query("SELECT urd FROM UserRoleDetails urd WHERE urd.user = :user AND urd.isActive = :isActive AND (urd.type = :type OR urd.type = :type2)")
     Optional<UserRoleDetails> findByUserAndIsActiveAndTypeExom(User user, boolean isActive, String type,String type2);
 }
